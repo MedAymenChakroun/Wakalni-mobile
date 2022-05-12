bb3f1636a252f68a62e9629386e2065ef4a19a77
Benalaya naim <98695786+Benalayanaim@users.noreply.github.com>
2022-05-13 01:44:57 +0200

    addleftover+organisation

---
+++ src/com/esprit/formulaire/gui/LeftoversListForm.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.formulaire.gui;
+
+/**
+ *
+ * @author dell
+ */
+
+import com.codename1.components.ImageViewer;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.EncodedImage;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Image;
+import com.codename1.ui.Label;
+import com.codename1.ui.Stroke;
+import com.codename1.ui.TextField;
+import com.codename1.ui.URLImage;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.plaf.RoundBorder;
+import com.esprit.formulaire.entities.Leftovers;
+import com.esprit.formulaire.services.LeftoversService;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class LeftoversListForm extends Form 
+{
+
+    public LeftoversListForm() {
+        
+                getToolbar().addMaterialCommandToSideMenu("LeftOvers", FontImage.MATERIAL_AV_TIMER, e ->  new LeftoversListForm().show() );
+                                 getToolbar().addMaterialCommandToSideMenu("Organisations", FontImage.MATERIAL_EVENT, e ->  new OrganisationListForm().show() );
+                
+               
+                setTitle("Leftovers");
+        getToolbar().addMaterialCommandToRightBar("", FontImage.MATERIAL_ADD, e -> {
+                new AddUpdateLeftoverForm(this,true,new Leftovers()).show();
+            
+        });
+                 
+        ArrayList<Leftovers> leftovers = LeftoversService.getInstance().getAllLeftovers();
+
+        
+        for (Leftovers l : leftovers) {
+         this.add(this.addleftoverHolder(l));
+        }
+                
+    }
+
+    private Container addleftoverHolder(Leftovers l) {
+        try {
+           
+            Container holderContainer = new Container(BoxLayout.x());
+            Container detailsContainer = new Container(BoxLayout.y());
+            Container buttonContainer = new Container(BoxLayout.x());
+
+           
+
+            Label lbSujet = new Label(l.getSujet());
+            Label lbAdresse = new Label(l.getType());
+            Button btUpdate = new Button("Modifier");
+            Button btDelete = new Button("Supprimer");
+            
+            btUpdate.getAllStyles().setBorder(RoundBorder.create().
+                    rectangle(true).
+                    color(0x228B22).
+                    strokeColor(0).
+                    strokeOpacity(120).
+                    stroke(new Stroke(2, Stroke.CAP_SQUARE, Stroke.JOIN_MITER, 1)));
+            btUpdate.getAllStyles().setFgColor(0xffffff);
+            
+            btDelete.getAllStyles().setBorder(RoundBorder.create().
+                    rectangle(true).
+                    color(0xFF0000).
+                    strokeColor(0).
+                    strokeOpacity(120).
+                    stroke(new Stroke(2, Stroke.CAP_SQUARE, Stroke.JOIN_MITER, 1)));
+            btDelete.getAllStyles().setFgColor(0xffffff);
+            
+            
+            buttonContainer.addAll(btUpdate, btDelete);
+            detailsContainer.addAll(lbSujet, lbAdresse,buttonContainer);
+            holderContainer.addAll(detailsContainer);
+
+            btUpdate.addActionListener(e -> {
+               new AddUpdateLeftoverForm(this,false,l).show();
+            });
+ 
+            btDelete.addActionListener(e -> {
+               LeftoversService.getInstance().deleteLeftover(l.getLeftoverid());
+                new LeftoversListForm().show();
+            });
+            
+            return holderContainer;
+            
+        } catch (NullPointerException e) {
+            System.err.println(e.getMessage());
+        } 
+        
+        return new Container(BoxLayout.x());
+    }
+
+  
+
+}
+
---
+++ src/com/esprit/formulaire/services/OrganisationService.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.formulaire.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import com.esprit.formulaire.entities.Organisation;
+import com.esprit.formulaire.utilis.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author dell
+ */
+public class OrganisationService {
+    
+    
+ public ArrayList<Organisation> Organisation;
+    public Organisation organisation;
+
+    public static OrganisationService instance = null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+
+    private OrganisationService() {
+        req = new ConnectionRequest();
+    }
+
+    public static OrganisationService getInstance() {
+        if (instance == null) {
+            instance = new OrganisationService();
+        }
+        return instance;
+    }
+
+    public ArrayList<Organisation> parseOrganisations(String jsonText) {
+        try {
+            Organisation = new ArrayList<>();
+            JSONParser j = new JSONParser();
+
+            Map<String, Object> organisationListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            List<Map<String, Object>> list = (List<Map<String, Object>>) organisationListJson.get("root");
+            
+            for (Map<String, Object> obj : list) {
+                
+                Organisation o = new Organisation();
+     
+                o.setOrganisationid((int) Float.parseFloat(obj.get("organisationid").toString()));
+                o.setNom(obj.get("nom").toString());
+                o.setEmail(obj.get("email").toString());
+                o.setAdresse(obj.get("adresse").toString());
+                o.setNumero((int)Float.parseFloat(obj.get("numero").toString()));
+               // l.setDateexpiration(obj.get("Dateexpiration").toString());
+             
+                Organisation.add(o);
+            }
+            
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        return Organisation;
+    }
+    
+    public ArrayList<Organisation> getAllOrganisations() {
+        String url = Statics.BASE_URL + "/api/organisations";
+        req.removeAllArguments();
+        req.setUrl(url);
+        req.setPost(false);
+        req.setHttpMethod("GET");
+        req.setFailSilently(true);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                Organisation = parseOrganisations(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return Organisation;
+    }
+    
+    
+    
+    public void addOrganisation(String nom, String email, String adresse, String numero, String leftoverid ) {
+        String url = Statics.BASE_URL + "/api/organisation/add";
+        req.removeAllArguments();
+        req.setUrl(url);
+        req.setPost(true);
+        req.setHttpMethod("POST");
+        req.addArgument("nom", nom);
+        req.addArgument("email",email);
+        req.addArgument("adresse", adresse);
+        req.addArgument("numero", numero);
+        req.addArgument("leftoverid", leftoverid);
+
+        
+        NetworkManager.getInstance().addToQueueAndWait(req);
+    }
+    
+    public void updateOrganisation(Integer Organisationid ,String nom, String email, String adresse, String numero) {
+        String url = Statics.BASE_URL + "/api/organisation/update";
+        req.removeAllArguments();
+        req.setUrl(url);
+        req.setPost(true);
+        req.setHttpMethod("POST");
+        //req.addArgument("username", MyApplication.loggedUser.getUsername());
+        req.addArgument("id", Organisationid.toString());
+        req.addArgument("nom", nom);
+        req.addArgument("email",email);
+        req.addArgument("adresse", adresse);
+        req.addArgument("numero", numero);
+        NetworkManager.getInstance().addToQueueAndWait(req);
+    }
+    
+    public void deleteOrganisation(Integer organisationid) {
+        String url = Statics.BASE_URL + "/api/organisation/delete";
+        req.removeAllArguments();
+        req.setUrl(url);
+        req.setPost(true);
+        req.setHttpMethod("POST");
+        //req.addArgument("username", MyApplication.loggedUser.getUsername());
+       
+        req.addArgument("organisationid", organisationid.toString());
+        NetworkManager.getInstance().addToQueueAndWait(req);
+    }
+    
+}
+
